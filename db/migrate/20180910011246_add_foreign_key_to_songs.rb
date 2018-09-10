055789389b6f00010c24c971a52d085dd2f47716
Christine Xiang <yutingcxiang@gmail.com>
2018-09-10 01:14:00 +0000

    automatically backed up by learn

--- db/migrate/20180910011246_add_foreign_key_to_songs.rb
+++ db/migrate/20180910011246_add_foreign_key_to_songs.rb
@@ -1,7 +1,5 @@
 class AddForeignKeyToSongs < ActiveRecord::Migration
   def change
-    change_table :songs do |t|
-      add_column :songs, :artist_id, :integer
-    end
+    add_column :songs, :artist_id, :integer
   end
 end
